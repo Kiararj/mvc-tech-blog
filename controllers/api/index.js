30c552f7c5284fb1c723f2c6e56a655a1bb6eb7c
Kiara Johnson <kiarajohnson91@gmail.com>
2023-10-28 17:35:37 -0500

    added home route, started api controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoute = require('./userRoute.js');
+const postRoute = require('./postRoute.js');
+const commentRoute = require('./commentRoute.js');
+
+router.use('/users', userRoute);
+router.use('/posts', postRoute);
+router.use('/comments', commentRoute)
+
+module.exports = router;
